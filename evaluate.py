fa0077f05996e60529f631ade8fb03aba817b1d6
julbujmoy <julbujmoy@gmail.com>
2025-03-06 13:04:28 +0100

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -12,7 +12,7 @@ cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-#cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # Solo "car" y "pedestrian"
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # Solo "car" y "pedestrian"
 cfg.MODEL.DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Cargar modelo
