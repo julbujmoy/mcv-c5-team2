6bc29f8e218a809f03ed75ecc124c223b8929093
laurasalort <laura.salort01@estudiant.upf.edu>
2025-03-10 00:49:52 +0100

    update files

--- week1/detectron/detectron_aug.py
+++ week1/detectron/detectron_aug.py
@@ -7,54 +7,49 @@ CUDA_VERSION = torch.__version__.split("+")[-1]
 print("torch: ", TORCH_VERSION, "; cuda: ", CUDA_VERSION)
 print("detectron2:", detectron2.__version__)
 
-# Some basic setup:
-# Setup detectron2 logger
 import detectron2
 from detectron2.utils.logger import setup_logger
 setup_logger()
 
-# import some common libraries
 import numpy as np
 import os, json, cv2, random
-
-# import some common detectron2 utilities
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-from detectron2.data import build_detection_test_loader
+from detectron2.data import DatasetMapper, build_detection_test_loader, build_detection_train_loader
 from detectron2.structures import BoxMode
 import albumentations as A
-from albumentations.pytorch import ToTensorV2
+from detectron2.utils.visualizer import ColorMode
+from detectron2.data import detection_utils as utils
+from detectron2.engine import DefaultTrainer
 
 
+#define data augmentation transforms
 transform = A.Compose([
     # A.CenterCrop(width=500,height=250),
     A.HorizontalFlip(p=0.5),
     A.RandomBrightnessContrast(p=0.2),
     A.Affine(translate_percent=0.05, scale=(0.8, 1.2), rotate=(-20, 20),shear=(-15,15), p=0.2),
 ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['category_id'],filter_invalid_bboxes=True))
+#we define the format of the BboxParams as pascal_voc because we have them in the format XYXY_ABS in the custom dataset we have created
 
-from detectron2.data import detection_utils as utils
 
 def custom_mapper(dataset_dict):
     """Apply Albumentations transforms on the fly"""
     dataset_dict = dataset_dict.copy()  # Avoid modifying the original dataset
     image = utils.read_image(dataset_dict["file_name"], format="BGR")  # Read image
-
-
     bboxes=[]
     category_id=[]
     for i in dataset_dict["annotations"]:
         bboxes.append(i['bbox'])
         category_id.append(i['category_id'])
+
     # Apply Albumentations augmentations
-    # print(dataset_dict)
     transformed = transform(image=image,bboxes=bboxes,category_id=category_id)
-    # print('TRANSFORMED')
-    # print(transformed)
+    
     dataset_dict["image"] = torch.as_tensor(transformed["image"].copy().transpose(2, 0, 1))  # Convert to tensor format
     
     annos=[]
@@ -63,36 +58,22 @@ def custom_mapper(dataset_dict):
                       'category_id':torch.as_tensor(transformed["category_id"][i],dtype=torch.int),
                       'bbox_mode':BoxMode.XYXY_ABS})
     
-   
-    # annos = [
-    #     utils.transform_instance_annotations(obj, transforms, image.shape[:2])
-    #     for obj in dataset_dict.pop("annotations")
-    #     if obj.get("iscrowd", 0) == 0
-    # ]
     instances = utils.annotations_to_instances(annos, transformed["image"].shape[:2])
     dataset_dict["instances"] = utils.filter_empty_instances(instances)
-    # print('INSTANCES')
-    # print(dataset_dict["instances"])
+
     return dataset_dict
 
-def get_balloon_dicts(img_dir,part):
+def get_KITTI_dicts(img_dir,part):
     json_file = os.path.join(img_dir,part+"_data_kitti_mots_coco.json")
     with open(json_file) as f:
         imgs_anns = json.load(f)
-
-    # for idx,v in enumerate(imgs_anns):
-    #   print(v)
 
     dataset_dicts = []
     for _, v in enumerate(imgs_anns):
         record = {}
         
-
         filename = os.path.join(img_dir,"image_02", v["image"])
           
-        # print(filename)
-        # height, width = cv2.imread(filename).shape[:2]
-
         record["file_name"] = filename
         record["image_id"] = v["image_id"]
         record["height"] = v["height"]
@@ -122,48 +103,40 @@ def get_balloon_dicts(img_dir,part):
 if __name__ == '__main__':
     
     for d in ["train","val"]:
-        DatasetCatalog.register("KITTI_" + d, lambda d=d: get_balloon_dicts("C:/Users/User/Documents/MASTER/c5/data_tracking_image_2/training/",d))
+        DatasetCatalog.register("KITTI_" + d, lambda d=d: get_KITTI_dicts("C:/Users/User/Documents/MASTER/c5/data_tracking_image_2/training/",d))
         MetadataCatalog.get("KITTI_" + d).set(thing_classes=["car","person"])
-    balloon_metadata = MetadataCatalog.get("KITTI_train")
+    KITTI_metadata = MetadataCatalog.get("KITTI_train")
     
     cfg = get_cfg()
-    # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
     cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-    # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
     predictor = DefaultPredictor(cfg)
-    # outputs = predictor(im)
 
 
-    dataset_dicts = get_balloon_dicts("C:/Users/User/Documents/MASTER/c5/data_tracking_image_2/training/","val")
+    ## predict a few images on the pretrained model to compare with finetuned results##
+
+    dataset_dicts = get_KITTI_dicts("C:/Users/User/Documents/MASTER/c5/data_tracking_image_2/training/","val")
     paths=[]
     for d in random.sample(dataset_dicts, 3):
         paths.append(d)
         
         print(d["file_name"])
         im = cv2.imread(d["file_name"])
-        outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
-        # We can use `Visualizer` to draw the predictions on the image.
+        outputs = predictor(im)  
         v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
         out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        # cv2.imshow('v',out.get_image()[:, :, ::-1])        # out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        # print(os.path.join("./output/",d["file_name"].split('/')[-2]+d["file_name"].split('/')[-1]))
         cv2.imwrite(os.path.join("./output/",d["file_name"].split('/')[-2])+'_'+d["file_name"].split('/')[-1],np.array(out.get_image()[:, :, ::-1]))
 
 
 
     ## TRAINING ##
 
-    from detectron2.engine import DefaultTrainer
-    from detectron2.data import DatasetMapper, build_detection_train_loader
-
     class AugmentedTrainer(DefaultTrainer):
         @classmethod
         def build_train_loader(cls, cfg):
             return build_detection_train_loader(cfg, mapper=custom_mapper)
 
-    # train_loader = build_detection_train_loader(cfg, mapper=custom_mapper)
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
     cfg.DATASETS.TRAIN = ("KITTI_train",)
@@ -182,34 +155,29 @@ if __name__ == '__main__':
     trainer.train()
 
 
-    # Inference should use the config with parameters that are used in training
-    # cfg now already contains everything we've set previously. We changed it a little bit for inference:
     cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set a custom testing threshold
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5 
     predictor = DefaultPredictor(cfg)
 
-    from detectron2.utils.visualizer import ColorMode
-    dataset_dicts = get_balloon_dicts("C:/Users/User/Documents/MASTER/c5/data_tracking_image_2/training/","val")
+
+    #run inference on same images as for pretrained model at the beggining #
+
+    dataset_dicts = get_KITTI_dicts("C:/Users/User/Documents/MASTER/c5/data_tracking_image_2/training/","val")
     for d in paths:
         
-        
-        # print(d["file_name"])
         im = cv2.imread(d["file_name"])
-        outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
-        # We can use `Visualizer` to draw the predictions on the image.
+        outputs = predictor(im)  
         v = Visualizer(im[:, :, ::-1],
-                    metadata=balloon_metadata,
+                    metadata=KITTI_metadata,
                     scale=0.5,
-                    instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+                    instance_mode=ColorMode.IMAGE_BW
         )
         out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
         print(os.path.join("./output/",'FINETUNED_flip_'+d["file_name"].split('/')[-2]+'_'+d["file_name"].split('/')[-1]))
         cv2.imwrite(os.path.join("./output/",'FINETUNED_flip_'+d["file_name"].split('/')[-2]+'_'+d["file_name"].split('/')[-1]),np.array(out.get_image()[:, :, ::-1]))
 
-    from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-    from detectron2.data import build_detection_test_loader
+
     evaluator = COCOEvaluator("KITTI_val", output_dir="./output")
     val_loader = build_detection_test_loader(cfg, "KITTI_val")
     print(inference_on_dataset(predictor.model, val_loader, evaluator))
-    # another equivalent way to evaluate the model is to use `trainer.test`
 
--- week1/detectron/detectron_pretrained.py
+++ week1/detectron/detectron_pretrained.py
@@ -7,27 +7,18 @@ CUDA_VERSION = torch.__version__.split("+")[-1]
 print("torch: ", TORCH_VERSION, "; cuda: ", CUDA_VERSION)
 print("detectron2:", detectron2.__version__)
 
-# Some basic setup:
-# Setup detectron2 logger
 import detectron2
 from detectron2.utils.logger import setup_logger
 setup_logger()
 
-# import some common libraries
 import numpy as np
 import os, json, cv2, random
-
-# import some common detectron2 utilities
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-from detectron2.data import build_detection_test_loader
 from detectron2.structures import BoxMode
-
-#/gpfs/home/lsalort/mcv-c5-team2/week1/detectron/data_tracking_image_2/training/
 
 
 def get_KITTI_dicts(img_dir,part):
@@ -35,19 +26,11 @@ def get_KITTI_dicts(img_dir,part):
     with open(json_file) as f:
         imgs_anns = json.load(f)
 
-    # for idx,v in enumerate(imgs_anns):
-    #   print(v)
-
     dataset_dicts = []
     for _, v in enumerate(imgs_anns):
         record = {}
         
-
         filename = os.path.join(img_dir,"image_02", v["image"])
-          
-        # print(filename)
-        # height, width = cv2.imread(filename).shape[:2]
-
         record["file_name"] = filename
         record["image_id"] = v["image_id"]
         record["height"] = v["height"]
@@ -60,7 +43,7 @@ def get_KITTI_dicts(img_dir,part):
             if annos["category"][i]==10: #ignore
                 continue
             elif  annos["category"][i]==1: #cars
-                cat=2
+                cat=2 #it is 2 to correspond with coco labels
             else: #person
                 cat=0
 
@@ -82,31 +65,24 @@ if __name__ == '__main__':
     KITTI_metadata = MetadataCatalog.get("KITTI_train")
     
     cfg = get_cfg()
-    # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
     cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-    # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  
+    
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
     predictor = DefaultPredictor(cfg)
-    # outputs = predictor(im)
 
 
     ## INFERENCE OFF-THE-SHELF ##
-
-    # evaluator = COCOEvaluator("KITTI_val", output_dir="./output")
-    # val_loader = build_detection_test_loader(cfg, "KITTI_val",)
-    # print(inference_on_dataset(predictor.model, val_loader, evaluator))
 
 
     dataset_dicts = get_KITTI_dicts("C:/Users/User/Documents/MASTER/c5/data_tracking_image_2/training/","val")
     for d in random.sample(dataset_dicts, 3):        
         print(d["file_name"])
         im = cv2.imread(d["file_name"])
-        outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
-        # We can use `Visualizer` to draw the predictions on the image.
+        outputs = predictor(im)  
+        # Use `Visualizer` to draw the predictions on the image.
         v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
         out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        # print(os.path.join("./output/",d["file_name"].split('/')[-2]+d["file_name"].split('/')[-1]))
         cv2.imwrite(os.path.join("./output/",d["file_name"].split('/')[-2])+'_'+d["file_name"].split('/')[-1],np.array(out.get_image()[:, :, ::-1]))
 
     if os.path.exists("predo.txt"):
@@ -119,22 +95,20 @@ if __name__ == '__main__':
         all_pred_boxes = []
         for item in loaded_data:
             restored_item = {
-                'boxes': torch.tensor(item['boxes']),  # Convert list back to tensor
+                'boxes': torch.tensor(item['boxes']), 
                 'labels': torch.tensor(item['labels'],dtype=torch.int),
-                'scores': torch.tensor(item['scores'])  # Convert list back to tensor
+                'scores': torch.tensor(item['scores'])  
             }
             all_pred_boxes.append(restored_item)
         
-        # Read the JSON file
         with open("gt.txt", "r") as f:
             loaded_data = json.load(f)
 
-        # Convert lists back to PyTorch tensors
         all_gt_boxes = []
         for item in loaded_data:
             restored_item = {
-                'boxes': torch.tensor(item['boxes']),  # Convert list back to tensor
-                'labels': torch.tensor(item['labels'],dtype=torch.int)  # Convert list back to tensor
+                'boxes': torch.tensor(item['boxes']),  
+                'labels': torch.tensor(item['labels'],dtype=torch.int) 
             }
             all_gt_boxes.append(restored_item)
 
@@ -150,7 +124,7 @@ if __name__ == '__main__':
             all_gt_boxes.append(
                     {"boxes": torch.Tensor(box), "labels": torch.Tensor(labels,dtpye=torch.int)})
             im = cv2.imread(d["file_name"])
-            outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+            outputs = predictor(im)  
             all_pred_boxes.append({"boxes": torch.Tensor(outputs['instances'].pred_boxes.tensor.tolist()), "labels": torch.Tensor(outputs['instances'].pred_classes.tolist(),dtype=torch.int), "scores": torch.Tensor(outputs['instances'].scores.tolist())})
             
             
@@ -162,6 +136,7 @@ if __name__ == '__main__':
                 'scores': item['scores'].tolist()
             }
             converted_data.append(converted_item)
+        
         # Save to a file
         with open("pred.txt", "w") as f:
             json.dump(converted_data, f)
